0f2ff83c145d60477f048d6e15fdb604760799d3
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-04-17 11:46:54 -0300

    Instalando o tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Cabecalho from "./components/Cabecalho";
 
 export default function App() {
   return (
     <>
-      <h1>React App</h1>
+      <Cabecalho />
     </>
   );
 }
--- src/components/Cabecalho/index.jsx
+++ src/components/Cabecalho/index.jsx
@@ -2,17 +2,20 @@ import "./style.css";
 
 export default function Cabecalho() {
   return (
-    <header>
+    <>
+      <header>
       
-    </header>
-    <nav>
-      <ul>
-        <li><a href="/">OLEOS ESSENCIAIS</a></li>
-        <li><a href="/">OLEOS VEGETAIS</a></li>
-        <li><a href="/">SINERGIAS</a></li>
-        <li><a href="/">COSMETICOS NATURAIS</a></li>
-        <li><a href="/">HIGIENE PESSOAL</a></li>
-        <li><a href="/">ACESSORIOS</a></li>
-    </nav>
+      </header>
+      <nav>
+        <ul>
+          <li><a href="/">OLEOS ESSENCIAIS</a></li>
+          <li><a href="/">OLEOS VEGETAIS</a></li>
+          <li><a href="/">SINERGIAS</a></li>
+          <li><a href="/">COSMETICOS NATURAIS</a></li>
+          <li><a href="/">HIGIENE PESSOAL</a></li>
+          <li><a href="/">ACESSORIOS</a></li>
+        </ul>
+      </nav>
+    </>
   );
 }
